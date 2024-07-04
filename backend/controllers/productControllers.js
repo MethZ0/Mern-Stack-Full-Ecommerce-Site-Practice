102162814def72f085d216b8bea23ab6d405920e
Methush Anjula <methushanjula01@gmail.com>
2024-07-04 18:31:00 +0530

    Backend Error Handling

---
+++ backend/controllers/productControllers.js
@@ -0,0 +1,72 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import Product from "../models/product.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+// Create new Product => /api/v1/products
+export const getProducts = catchAsyncErrors(async (req, res) => {
+    const products = await Product.find();
+
+    res.status(200).json({
+        products,
+    });
+});
+
+
+// Create new Product => /api/v1/admin/products
+export const newProduct = catchAsyncErrors(async (req, res) => {
+    
+    const product = await Product.create(req.body)
+
+    res.status(200).json({
+        product,
+    });
+});
+
+// Get Single Product Details => /api/v1/products/:id
+export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
+    
+    const product = await Product.findById(req?.params?.id)
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    res.status(200).json({
+        product,
+    });
+});
+
+// Update Product Details => /api/v1/products/:id
+export const updateProduct = catchAsyncErrors(async (req, res) => {
+    
+    let product = await Product.findById(req?.params?.id)
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { 
+        new: true 
+    });
+
+    res.status(200).json({
+        product,
+    });
+});
+
+// Delete Product Details => /api/v1/products/:id
+export const deleteProduct = catchAsyncErrors(async (req, res) => {
+    
+    const product = await Product.findById(req?.params?.id)
+
+    if(!product){
+        return next(new ErrorHandler("Product not found", 404));
+    }
+
+    await product.deleteOne();
+
+
+    res.status(200).json({
+        message: "Product Deleted",
+    });
+});
